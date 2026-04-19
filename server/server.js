d292c22e2f141ef4ce8a6764b4421a1dc83ee27e
Om Pandey <ompandey628782@gmail.com>
2026-04-19 15:51:51 +0530

    routing-add-Backend

--- server/server.js
+++ server/server.js
@@ -292,7 +292,7 @@ if (process.env.NODE_ENV === 'production') {
   const __dirname = path.dirname(fileURLToPath(import.meta.url));
   app.use(express.static(path.join(__dirname, '../client/dist')));
   
-  app.get('*', (req, res) => {
+  app.get('/(.*)', (req, res) => {
     res.sendFile(path.join(__dirname, '../client/dist/index.html'));
   });
 }
